bfa47653aee0c4899e6f7d861730aeca5f5407f4
dponomareva <daraonelove@yandex.ru>
2022-03-03 11:19:39 +0300

    auth fix

--- app/src/main/java/com/dxshulya/wasabi/model/Authorization.kt
+++ app/src/main/java/com/dxshulya/wasabi/model/Authorization.kt
@@ -2,7 +2,7 @@ package com.dxshulya.wasabi.model
 
 data class Authorization(
     val statusCode: Int,
-    val message: List<String>,
+    val message: String,
     val error: String,
     val token: String,
     val login: String
