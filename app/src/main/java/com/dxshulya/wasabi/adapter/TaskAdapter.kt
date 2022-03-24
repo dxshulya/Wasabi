dee606f9b92ce29d3dc832f0abb7415dadce5cff
dxshulya <daraoneove@yandex.ru>
2022-03-24 16:44:57 +0300

    change adapter + new logo

--- app/src/main/java/com/dxshulya/wasabi/adapter/TaskAdapter.kt
+++ app/src/main/java/com/dxshulya/wasabi/adapter/TaskAdapter.kt
@@ -1,11 +1,16 @@
 package com.dxshulya.wasabi.adapter
 
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.dxshulya.wasabi.R
+import com.dxshulya.wasabi.databinding.TaskItemBinding
 import com.dxshulya.wasabi.model.Task
 
-class TaskAdapter : PagingDataAdapter<Task, TaskViewHolder>(COMPARATOR) {
+class TaskAdapter : PagingDataAdapter<Task, TaskAdapter.TaskViewHolder>(COMPARATOR) {
 
     companion object {
         private val COMPARATOR = object : DiffUtil.ItemCallback<Task>() {
@@ -18,11 +23,43 @@ class TaskAdapter : PagingDataAdapter<Task, TaskViewHolder>(COMPARATOR) {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-        return TaskViewHolder.create(parent)
+        val binding = TaskItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TaskViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        val taskItem = getItem(position)
-        holder.bind(taskItem)
+        val currentItem = getItem(position)
+        if (currentItem != null) {
+            holder.bind(currentItem)
+        }
+    }
+
+    inner class TaskViewHolder(private val binding: TaskItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        @SuppressLint("SetTextI18n")
+        fun bind(task: Task) {
+            showLikes(task)
+            with(binding) {
+                taskNumber.text = "№ " + (position + 1)
+                taskFormula.text = task.formula
+                taskText.text = task.text
+                taskAnswer.text = "Ответ: " + task.answer
+                with(taskLike) {
+                    setOnClickListener {
+                        task.isLiked = true
+                        showLikes(task)
+                    }
+                }
+            }
+        }
+
+        private fun showLikes(task: Task) {
+            if (task.isLiked) {
+                binding.taskLike.setImageResource(R.drawable.ic_favorite)
+            } else {
+                binding.taskLike.setImageResource(R.drawable.ic_favorite_border)
+            }
+        }
     }
 }
