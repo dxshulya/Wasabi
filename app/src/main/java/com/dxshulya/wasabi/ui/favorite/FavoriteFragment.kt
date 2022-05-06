861480f32b3d8b4d86fde51ade361e6c65900dc5
dxshulya <daraonelove@yandex.ru>
2022-05-06 18:41:21 +0300

    fix ui

--- app/src/main/java/com/dxshulya/wasabi/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/dxshulya/wasabi/ui/favorite/FavoriteFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -15,6 +16,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.dxshulya.wasabi.R
 import com.dxshulya.wasabi.adapter.FavoriteAdapter
 import com.dxshulya.wasabi.databinding.FavoriteFragmentBinding
+import com.google.android.material.progressindicator.CircularProgressIndicator
 
 class FavoriteFragment : Fragment() {
 
@@ -31,12 +33,14 @@ class FavoriteFragment : Fragment() {
     private lateinit var favoriteRefresh: SwipeRefreshLayout
     private lateinit var emptyView: TextView
     private lateinit var emptyText: TextView
+    private lateinit var favoriteBar: CircularProgressIndicator
 
     private fun initUis() {
         favoriteRecycler = binding.favouriteRecycler
         favoriteRefresh = binding.favoriteRefresh
         emptyView = binding.emptyView
         emptyText = binding.emptyText
+        favoriteBar = binding.favoriteBar
     }
 
     override fun onCreateView(
@@ -55,6 +59,7 @@ class FavoriteFragment : Fragment() {
 
         favoriteRefresh.setOnRefreshListener {
             favoriteRefresh.isRefreshing = false
+            favoriteRecycler.adapter = FavoriteAdapter(viewLifecycleOwner).also { favoriteAdapter = it }
             viewModel.getFavorites()
         }
 
@@ -64,6 +69,22 @@ class FavoriteFragment : Fragment() {
             if (it.append is LoadState.NotLoading && it.append.endOfPaginationReached) {
                 emptyView.isVisible = favoriteAdapter?.itemCount!! < 1
                 emptyText.isVisible = favoriteAdapter?.itemCount!! < 1
+            }
+
+            if (it.refresh is LoadState.Loading) {
+                favoriteBar.visibility = View.VISIBLE
+            }
+            else {
+                favoriteBar.visibility = View.GONE
+
+                val errorState = when {
+                    it.append is LoadState.Error -> it.append as LoadState.Error
+                    it.prepend is LoadState.Error -> it.prepend as LoadState.Error
+                    else -> null
+                }
+                errorState?.let { e ->
+                    Toast.makeText(binding.root.context, e.error.message, Toast.LENGTH_SHORT).show()
+                }
             }
         }
 
